be4a250478156beed822a6643a075f5cf996ebd4
Jamie Heather <james.m.heather@gmail.com>
2023-03-03 11:19:16 -0500

    2022-03-03: v0.4.0 repo, 0.3.0 code. Added allele naming and rudimentary sequence notifications (largely due to receptor_utils). Fixed minor bug with spaces in a Rodriguez allele.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Novel TCR alleles
-## version 0.3.0 
+## version 0.4.0 
 ### JH @ MGH, 2023
 
 This repo aims to gather together published novel human TCR gene alleles as-yet not included in IMGT/GENE-DB, either inferred from full-variable domain spanning TCR-seq reads, observed directly in long-read genomic DNA, or from some similar technology that doesn't rely on assembling full regions from short reads. This may prove useful for TCRseq analysis applications, or for quick reference for efforts such as the [AIRR-C's Inferred Allele Review Committee](https://www.antibodysociety.org/the-airr-community/airr-subcomittees/inferred-allele-review-committee-iarc/), which aim to more rigorously update the field's germline knowledge.
@@ -19,6 +19,12 @@ Currently this table collates alleles from:
 * The differences in naming between the alleles found in both the Heather and Omer/Peres datasets is on account of me naming the ones I discovered using ungapped sequence positions, and them using gapped.
 * The Corcoran *et al.* study includes five sets of monozygotic twins, which might affect consideration of the number of donors a given allele occurs in.  
 
+### Prerequisites
+
+* `python3.10` or greater
+  * `biopython`
+  * `receptor_utils` (accessible in the user's PATH)
+
 ### Compiling the data
 
 The table can be generated using any combination of input files, provided they are supplied in the appropriate format and location. A single tab-separated file should be placed in the `input-data/` directory for each dataset to be included. Note that the name of these files will be used for column headers, so should be sensibly named. The fields that these files must contain are laid out in the `input-data/input-template.tsv` file. The fields, which should all have a value provided, are:
@@ -34,6 +40,13 @@ The table can be generated using any combination of input files, provided they a
 * Number-Donors-Searched
   * The total number of individuals that were screened in the assay where the allele was detected (label '1' if not known)
 
-Running `compile-alleles.py` in the `scripts/` dir will read in all of the suitably provided input files, and concatenate them together, outputting a table where each row is a unique novel allele, with columns annotating the relevant details from the datasets in which they appear. This output file will appear in the root directory, named in the format `novel-TCR-alleles-YYYY-MM-DD-vX.X.X.tsv`, based on the date of running and the version of the repo, which should increment per changes to the input data. Any existing output files fitting that format in the root directory will be moved to the `archive/` directory.
+The table aims to:
+* Combine the novel alleles presented in each input study into a single document, allowing a quick way to see how many of these studies each alleles occur in. Additionally, where possible, it aims to include information as to how many donors each allele was found in, and what identifiers they were referred to in each paper.
+* Determine whether and when a novel allele may have been identified and published in an official IMGT release (potentially in a truncated form). This is done by comparing the sequences with the [historical entries of GENE-DB collated here](https://github.com/JamieHeather/genedb-releases).
+* Assign a simple convenient name for the novel allele, in which it is named after the most closely related named IMGT allele, suffixed with information as to the polymorphisms which distinguish them. This is based on the [principles outlined here](https://wordpress.vdjbase.org/index.php/vdjbase_help/airr-seq-data-allele-names/) and achieved using William Lees' [`receptor_utils` package](https://williamdlees.github.io/receptor_utils/_build/html/index.html). This naming is calculated relative to the most recent IMGT/GENE-DB entry in the `genedb-releases` submodule, and therefore may be subject to change.
+  * Note that this process does not work for a small number of alleles for which there isn't a suitable reference in the IMGT release used, prompting a `No reference genes to compare` error from `receptor_utils`. This mostly seems to occur for out-of-frame pseudogenes, which gapped sequences are not available for.
+* The `receptor_utils` package also provides other useful information, such as IMGT gapped sequences and warnings when novel alleles are missing conserved residues; this information is also retained in the output table.
+
+Running `compile-alleles.py` in the `scripts/` dir will read in all of the suitably provided input files, and concatenate them together, outputting a table where each row is a unique novel allele, with columns annotating the relevant details from the datasets in which they appear. This output file will appear in the root directory, named in the format `novel-TCR-alleles_YYYY-MM-DD_vX.X.X_GENEDB-XXXXX-X.tsv`, based on the date of running, the version of the repo (which should increment per changes to the input data or table generation code), and the [IMGT/GENE-DB release](https://www.imgt.org/download/GENE-DB/) used as a comparison. Any existing output files fitting that format in the root directory will be moved to the `archive/` directory.
 
 Note that apart from selection of studies, no specific QC or additional validation has been applied to the sequences in question. The summary file does however contain columns of total numbers of datasets and donors each sequence was observed in.
--- scripts/compile-alleles.py
+++ scripts/compile-alleles.py
@@ -10,9 +10,10 @@ import os
 import collections as coll
 import pandas as pd
 import datetime
+import subprocess
 
 __email__ = 'jheather@mgh.harvard.edu'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 __author__ = 'Jamie Heather'
 
 
@@ -44,6 +45,44 @@ def today():
     :return: Today's day, in ISO format
     """
     return datetime.datetime.today().date().isoformat()
+
+
+def read_fa(ff):
+    """
+    :param ff: opened fasta file
+    read_fa(file):Heng Li's Python implementation of his readfq function (tweaked to only bother with fasta)
+    https://github.com/lh3/readfq/blob/master/readfq.py
+    """
+
+    last = None                                 # this is a buffer keeping the last unprocessed line
+    while True:                                 # mimic closure
+        if not last:                            # the first record or a record following a fastq
+            for l in ff:                        # search for the start of the next record
+                if l[0] in '>':                 # fasta header line
+                    last = l[:-1]               # save this line
+                    break
+        if not last:
+            break
+        name, seqs, last = last[1:], [], None
+        for l in ff:                            # read the sequence
+            if l[0] in '>':
+                last = l[:-1]
+                break
+            seqs.append(l[:-1])
+        if not last or last[0] != '+':          # this is a fasta record
+            yield name, ''.join(seqs), None     # yield a fasta record
+            if not last:
+                break
+        else:
+            raise IOError("Input file does not appear to be a FASTA file - please check and try again")
+
+
+def flatten(list_of_lists):
+    """
+    :param list_of_lists: List of lists, as may be generated at certain parts of the analysis
+    :return: Said list of lists flattened into a single one-level deep list
+    """
+    return [thing for sublist in list_of_lists for thing in sublist]
 
 
 # Detect data files
@@ -133,8 +172,124 @@ for ov in older_versions:
     else:
         os.replace('../' + ov, '../archive/' + ov.replace('.tsv', '-name-clash.tsv'))
 
-# Then stick it all together and write it out
+# Then stick it all together into a table before further processing
 out_dat = list_to_df(out_dat, out_headers, False)
-out_nam = out_prefix + today() + '-v' + run_version + '.tsv'
 
+# Need to determine whether the alleles are featured in IMGT/GENE-DB, and if so when
+# Go through the genedb-releases scrapes, and compile allele-specific release data
+release_path = '../genedb-releases/releases/'
+release_dirs = [x for x in os.listdir(release_path) if os.path.isdir(release_path + x) and x.startswith('20')]
+release_dirs.sort()
+genedb = coll.defaultdict(list)
+partial = coll.defaultdict()
+for rdir in release_dirs:
+    release = rdir.split('_')[-1]
+    genedb_file = [x for x in os.listdir(release_path + rdir) if 'fasta-nt-WithoutGaps-F+ORF+allP' in x]
+    if len(genedb_file) != 1:
+        raise IOError('Unexpected number of file matches in release folder ' + rdir)
+    else:
+        with open(release_path + rdir + '/' + genedb_file[0], 'r') as in_file:
+            for header, seq, qualnull in read_fa(in_file):
+                # Take human TCR V/J genes
+                if 'Homo sapiens' not in header:
+                    continue
+                bits = header.split('|')
+                if bits[1][:2] != 'TR' or bits[1][3] not in ['V', 'J']:
+                    continue
+                seq = seq.upper()
+                genedb[seq].append(bits[1] + '|' + release)
+                if bits[13] != ' ':
+                    partial[bits[1]] = bits[13].strip()
+
+# Then go back through our novel alleles, and see if any appear in a previous release
+# Simultaneously also use William's name_allele utility to generate a standardised name, using most recent IMGT release
+
+# First generate the necessary reference files
+gapped_file = release_path + rdir + '/IMGTGENEDB-ReferenceSequences.fasta-nt-WithGaps-F+ORF+inframeP'
+for locus in ['A', 'B', 'G', 'D']:
+    cmd = 'extract_refs -L TR' + locus + ' ' + gapped_file + ' "Homo sapiens"'
+    subprocess.call(cmd, shell=True)
+
+# Then iterate across the dataframe
+imgt_release = []
+imgt_id = []
+std_id = []
+gapped = []
+notes = []
+file_suffix = {'V': '_gapped.fasta ', 'J': '.fasta '}
+
+# Check whether each 'novel' allele is actually an existing covered IMGT gene
+for na in out_dat.index:
+    # Check for previous appearances
+    seq = out_dat.loc[na]['Ungapped-Sequence']
+    tmp_imgt_id, tmp_imgt_release, note, ids = [''] * 4
+
+    # First check for exact matches ...
+    if seq in genedb:
+        ids = list(set([x.split('|')[0] for x in genedb[seq]]))
+        releases = list(set([x.split('|')[1] for x in genedb[seq]]))
+        note += 'Exact match of '
+
+    # ... Then check whether the novel allele is a substring of any IMGT allele ...
+    elif len([x for x in genedb if seq in x]) > 0:
+        ids = list(set([x.split('|')[0] for x in flatten([genedb[x] for x in genedb if seq in x])]))
+        releases = list(set([x.split('|')[1] for x in flatten([genedb[x] for x in genedb if seq in x])]))
+        note += 'Shorter version of '
+
+    # ... Or whether any IMGT allele is a substring of it
+    elif len([x for x in genedb if x in seq]) > 0:
+        ids = list(set([x.split('|')[0] for x in flatten([genedb[x] for x in genedb if x in seq])]))
+        releases = list(set([x.split('|')[1] for x in flatten([genedb[x] for x in genedb if x in seq])]))
+        note += 'Longer version of '
+
+    # Account for potential multiple matches
+    if ids:
+        ids.sort()
+        tmp_imgt_id = '|'.join(ids)
+        note += tmp_imgt_id
+        if tmp_imgt_id in partial:
+            note += ' (' + partial[tmp_imgt_id] + '). '
+        else:
+            note += '. '
+
+        releases.sort()
+        tmp_imgt_release = releases[0]
+
+    imgt_release.append(tmp_imgt_release)
+    imgt_id.append(tmp_imgt_id)
+
+    # Then generate standardised naming (off this release)
+    gene = out_dat.loc[na]['Gene']
+    cmd = 'name_allele -g ' + gene + ' Homo_sapiens_' + gene[:4] + file_suffix[gene[3]] + seq
+
+    naming = subprocess.check_output(cmd, shell=True, stderr=subprocess.DEVNULL).decode('UTF-8').rstrip()
+    if '\n' in naming:
+        naming_bits = naming.split('\n')
+        if len(naming_bits) in [3, 5]:
+            std_id.append(naming_bits[0].upper())
+            gapped.append(naming_bits[2])
+            if len(naming_bits) == 5:
+                note += naming_bits[4] + '. '
+        else:
+            raise IOError("Unknown format")
+    else:
+        gapped.append('')
+        if naming == 'No reference genes to compare':
+            std_id.append('-')
+        else:
+            std_id.append(naming.upper())
+
+    notes.append(note)
+
+# Add those new columns back in, and write the updated table out
+out_dat.insert(1, 'IMGT-ID', imgt_id)
+out_dat.insert(1, 'IMGT-Appearance', imgt_release)
+out_dat.insert(1, 'Standard-ID', std_id)
+out_dat.insert(5, 'Gapped-Sequence', gapped)
+out_dat.insert(4, 'Notes', notes)
+
+out_nam = out_prefix + '_'.join([today(), 'v' + run_version, 'GENEDB-' + release + '.tsv'])
 out_dat.to_csv('../' + out_nam, sep='\t', index=False)
+
+# ... and tidy up the references
+subprocess.call('rm Homo*fasta', shell=True)
